8e7dbde1df48261497ca9e95351cef6a028c5a34
sakshi <panchalsakshi01@gmail.com>
2024-11-28 04:24:28 -0800

    routing using react-router dom

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,35 @@
 import '../styles/App.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// routing for websites goes here....
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Root Element</div>
+  },
+  {
+    path: '/quiz',
+    element: <div>Quiz</div>
+  },
+  {
+    path: '/result',
+    element: <div>Result</div>
+  },
+  {path: '/login',
+    element: <div>Login</div>
+  },
+  {
+    path: '/signup',
+    element: <div>Signup</div>
+  }
+])
+
 function App() {
 
   return (
     <>
-      <div>Home Page</div>
+      <RouterProvider router={router} />
     </>
   )
 }
